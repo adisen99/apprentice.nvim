c80eed9996144898a71c6916d511e7de1a3d7a90
adisen99 <adityasenguptasai99@gmail.com>
2021-09-22 10:30:44 +0530

    fix: better neogit highlights added

--- lua/apprentice/plugins/neogit.lua
+++ lua/apprentice/plugins/neogit.lua
@@ -1,6 +1,24 @@
--- buftabline highlights
+-- neogit highlights
 local lush = require("lush")
-local base = require('codeschool.base')
+local base = require('apprentice.base')
+local colors = require('apprentice.colors')
+
+local neogitaddfg = colors.bright_green
+local neogitdelfg = colors.bright_red
+
+if vim.o.background == 'dark' then
+  Neogitbg1 = colors.dark2
+  Neogitbg2 = colors.dark3
+  Neogitbg3 = colors.dark4
+  Neogitfg1 = colors.light3
+  Neogitfg2 = colors.light2
+elseif vim.o.background == 'light' then
+  Neogitbg1 = colors.light2
+  Neogitbg2 = colors.light3
+  Neogitbg3 = colors.light4
+  Neogitfg1 = colors.dark2
+  Neogitfg2 = colors.dark3
+end
 
 local M = {}
 
@@ -9,12 +27,12 @@ M = lush(function()
     -- Neogit
   NeogitNotificationInfo {base.CodeschoolGreen},
   NeogitNotificationWarning {base.CodeschoolYellow},
-  NeogitNotificationError {base.CodeschoolRed},
-  NeogitDiffAddHighlight {base.CodeschoolGreenSign},
-  NeogitDiffDeleteHighlight {base.CodeschoolRedSign},
-  NeogitDiffContextHighlight {base.CodeschoolFg3},
-  NeogitHunkHeader {base.CodeschoolFg1},
-  NeogitHunkHeaderHighlight {base.CodeschoolFg1},
+  NeogitNotificationError {fg = colors.faded_red},
+  NeogitDiffAddHighlight {bg = Neogitbg2, fg = neogitaddfg},
+  NeogitDiffDeleteHighlight {bg = Neogitbg2, fg = neogitdelfg},
+  NeogitDiffContextHighlight {bg = Neogitbg1, fg = Neogitfg1},
+  NeogitHunkHeader {bg = Neogitbg2, fg = Neogitfg2},
+  NeogitHunkHeaderHighlight {bg = Neogitbg3, fg = Neogitfg2},
 	}
 end)
 
