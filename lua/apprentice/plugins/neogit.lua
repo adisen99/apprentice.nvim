1e23ca789e399c570df4150aa1d2c60ae10f2907
adisen99 <adityasenguptasai99@gmail.com>
2021-09-22 10:39:43 +0530

    test: only dark mode support for neogit

--- lua/apprentice/plugins/neogit.lua
+++ lua/apprentice/plugins/neogit.lua
@@ -6,19 +6,19 @@ local colors = require('apprentice.colors')
 local neogitaddfg = colors.bright_green
 local neogitdelfg = colors.bright_red
 
-if vim.o.background == 'dark' then
-  Neogitbg1 = colors.dark2
-  Neogitbg2 = colors.dark3
-  Neogitbg3 = colors.dark4
-  Neogitfg1 = colors.light3
-  Neogitfg2 = colors.light2
-elseif vim.o.background == 'light' then
+local neogitbg1 = colors.dark2
+local neogitbg2 = colors.dark3
+local neogitbg3 = colors.dark4
+local neogitfg1 = colors.light3
+local neogitfg2 = colors.light2
+
+--[[ elseif vim.o.background == 'light' then
   Neogitbg1 = colors.light2
   Neogitbg2 = colors.light3
   Neogitbg3 = colors.light4
   Neogitfg1 = colors.dark2
   Neogitfg2 = colors.dark3
-end
+end ]]
 
 local M = {}
 
@@ -28,11 +28,11 @@ M = lush(function()
   NeogitNotificationInfo {base.CodeschoolGreen},
   NeogitNotificationWarning {base.CodeschoolYellow},
   NeogitNotificationError {fg = colors.faded_red},
-  NeogitDiffAddHighlight {bg = Neogitbg2, fg = neogitaddfg},
-  NeogitDiffDeleteHighlight {bg = Neogitbg2, fg = neogitdelfg},
-  NeogitDiffContextHighlight {bg = Neogitbg1, fg = Neogitfg1},
-  NeogitHunkHeader {bg = Neogitbg2, fg = Neogitfg2},
-  NeogitHunkHeaderHighlight {bg = Neogitbg3, fg = Neogitfg2},
+  NeogitDiffAddHighlight {bg = neogitbg2, fg = neogitaddfg},
+  NeogitDiffDeleteHighlight {bg = neogitbg2, fg = neogitdelfg},
+  NeogitDiffContextHighlight {bg = neogitbg1, fg = neogitfg1},
+  NeogitHunkHeader {bg = neogitbg2, fg = neogitfg2},
+  NeogitHunkHeaderHighlight {bg = neogitbg3, fg = neogitfg2},
 	}
 end)
 
